742a1388eecb112c759669f11dfa17d7cb08d74c
Julian <juliantomczyk@gmail.com>
2021-10-20 12:01:44 +0200

    modif perso

--- js/personnages.js
+++ js/personnages.js
@@ -2,8 +2,16 @@ const API_URL = "https://swapi.dev/api/people/?page=";
 const CONTAINER = document.getElementById('container');
 const BUTTON_NEXT = document.getElementById('button-next');
 const BUTTON_BACK = document.getElementById('button-back');
+const BUTTON_SEARCH = document.getElementById('buttonData');
+const INPUT_FIELD = document.getElementById('inputField');
 
+let listButtonWorld = [];
+let listButtonMovie = [];
 let pageIndex = 1;
+
+BUTTON_SEARCH.addEventListener('click', () => {
+    search();
+})
 
 BUTTON_NEXT.addEventListener('click', () => {
     pageIndex++;
@@ -17,11 +25,11 @@ BUTTON_BACK.addEventListener('click', () => {
     createThumbnail();
 });
 
-async function getData(id) {
+async function getData(id, homeWorld, movies) {
     try {
         const response = await fetch(API_URL + pageIndex);
         const responseFormat = await response.json();
-        console.log(responseFormat);
+        //console.log(responseFormat);
 
         const NAME = document.getElementById('name'+id);
         const HEIGHT = document.getElementById('height'+id);
@@ -29,6 +37,7 @@ async function getData(id) {
         const DATE = document.getElementById('date'+id);
         const GENDER = document.getElementById('gender'+id);
         const HOMEWORLD = document.getElementById('homeworld'+id);
+        const MOVIES = document.getElementById('movies'+id);
 
         NAME.innerText = responseFormat.results[id].name;
         HEIGHT.innerText = responseFormat.results[id].height + "cm";
@@ -36,9 +45,21 @@ async function getData(id) {
         DATE.innerText = responseFormat.results[id].birth_year;
         GENDER.innerText = responseFormat.results[id].gender;
 
-        const responsePlanet = await fetch(responseFormat.results[id].homeworld);    
-        const responseFormatPlanet = await responsePlanet.json();
-        HOMEWORLD.innerText =  responseFormatPlanet.name;
+        if(homeWorld) {
+            const responsePlanet = await fetch(responseFormat.results[id].homeworld);    
+            const responseFormatPlanet = await responsePlanet.json();
+            HOMEWORLD.innerText =  responseFormatPlanet.name;
+        }
+
+        if(movies) {
+            const listMoviesURL = responseFormat.results[id].films;
+            const responseMovie = await Promise.all(listMoviesURL.map(x => fetch(x)));
+            const responseFormatMovie = await Promise.all(responseMovie.map(x => x.json()));
+            console.log(responseFormatMovie);
+            for(let index = 0; index < responseFormatMovie.length; index++) {
+                MOVIES.innerText += responseFormatMovie[index].title + "\n";
+            }
+        }
     }
     catch(err) {
         console.error(err);
@@ -49,22 +70,55 @@ function createThumbnail() {
     for(let index = 0; index < 10; index++) {
         CONTAINER.innerHTML += 
     `
-    <div class="thumbnail">
+    <div class="thumbnail" id="thumbnail${index}">
         <p id="name${index}">name : </p>
         <p id="height${index}">height : </p>
         <p id="mass${index}">mass : </p>
         <p id="date${index}">naissance : </p>
         <p id="gender${index}">sexe : </p>
-        <p id="homeworld${index}">monde : </p>
+        <button id="button-world${index}">Monde</button>
+        <p id="homeworld${index}"></p>
+        <button id="button-movies${index}">Movies</button>
+        <p id="movies${index}"></p>
     </div>
     `;
 
-    getData(index);
+    getData(index, false, false);
     }
+
+    setButtons();
 }
 
 function resetThumbnail() {
     CONTAINER.innerHTML = "";
+    listButtonWorld = [];
+    listButtonMovie = [];
+}
+
+function setButtons() {
+    for(let index = 0; index < 10; index++) {
+        const buttonWorld = new Object();
+        buttonWorld.ref = document.getElementById(`button-world${index}`);
+
+        const buttonMovie = new Object();
+        buttonMovie.ref = document.getElementById(`button-movies${index}`);
+
+        listButtonWorld.push(buttonWorld);
+        listButtonMovie.push(buttonMovie);
+    }
+
+    for(let index = 0; index < 10; index++) {
+        listButtonWorld[index].ref.addEventListener('click', () => {
+            getData(index, true, false);
+        });
+        listButtonMovie[index].ref.addEventListener('click', () => {
+            getData(index, false, true);
+        });
+    }
+}
+
+function search() {
+    
 }
 
 createThumbnail();
